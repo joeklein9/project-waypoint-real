6055652aeb36dfc0322e032e226ce77c4fc45c9f
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-03 09:42:48 -0500

    added submit button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from "react"
 
 export default function Navbar () {
     return (
-        <nav className = "navbar-container">Project Waypoint</nav>
+        <nav className = "navbar-container">
+            <h1 className = "navbar-text">Project Waypoint</h1>
+        </nav>
 
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
       <div className = "input-container">
         <input className = "link-input" placeHolder = "Enter spreadsheet URL here"></input>
+        <button className = "link-submit">Submit</button>
       </div>
       
       

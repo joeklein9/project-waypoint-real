6e7966e1e133d638c6c6e6537c5b58958f64c4c6
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-03 02:05:22 -0500

    styling landing page

---
+++ components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Navbar () {
+    return (
+        <nav className = "navbar-container">Project Waypoint</nav>
+
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from '/components/Navbar.jsx'
 
 
 function App() {
   const [count, setCount] = useState(10)
 
   return (
-    <div className="App">
-      <input className = "link-input" placeHolder = "Enter spreadsheet URL here"></input>
+    <div className="App">  
+      <Navbar />
+      <div className = "input-container">
+        <input className = "link-input" placeHolder = "Enter spreadsheet URL here"></input>
+      </div>
+      
+      
+
     </div>
   )
 }

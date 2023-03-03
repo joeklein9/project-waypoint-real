59fc755a1a699aad212bfd636e9c9be007948ccf
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-03 02:42:30 -0500

    removed unneeded boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from '/components/Navbar.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(10)
+
 
   return (
     <div className="App">  

ad000b422ed9ad27e20a45dc2348920901dde9ce
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-06 11:56:23 -0500

    changes

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,8 +1,25 @@
 import React from "react"
+import {useState} from "react"
+import drivers from "/data/drivers.js"
+ 
 
-export default function Collapsible () {
+export default function Collapsible (props) {
+    const [isOpen, setIsOpen] = useState (false)
+
+
+
+
     return (
-        <h1>Collapsible</h1>
+        <div className = "collapsible">
+            <button className = "toggle" onClick = {()=> setIsOpen(!isOpen)}>{drivers.name.}</button>
+            { isOpen && <p className = "content">Some content</p>}
+        
+        
+        
+        
+        </div>
+
+        
 
     )
 }

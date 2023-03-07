1c7c5156e670b1e641c23efaa9226bcdac94bc0f
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-06 23:08:27 -0500

    changes

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,68 +1,71 @@
-import React from "react"
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
-const mapStyles = {        
-    height: "75vh",
-    width: "75vh"};
+
+// import React from "react"
+// import { GoogleMap, LoadScript } from '@react-google-maps/api'
+
+// const mapStyles = {        
+//     height: "75vh",
+//     width: "75vh"};
   
-  const defaultCenter = {
-    lat: 33.753746, lng: -84.386330
-  }
+//   const defaultCenter = {
+//     lat: 33.753746, lng: -84.386330
+//   }
 
-  const locations = [
-    {
-      name: "Location 1",
-      location: { 
-        lat: 33.3954,
-        lng: -84 
-      },
-    },
-    {
-      name: "Location 2",
-      location: { 
-        lat: 33,
-        lng: -84.23
-      },
-    },
-    {
-      name: "Location 3",
-      location: { 
-        lat: 33.76,
-        lng: -84.38
-      },
-    },
-    {
-      name: "Location 4",
-      location: { 
-        lat: 41.3797,
-        lng: 2.1682
-      },
-    },
-    {
-      name: "Location 5",
-      location: { 
-        lat: 41.4055,
-        lng: 2.1915
-      },
-    }
-  ];
+//   const locations = [
+//     {
+//       name: "Location 1",
+//       location: { 
+//         lat: 33.3954,
+//         lng: -84 
+//       },
+//     },
+//     {
+//       name: "Location 2",
+//       location: { 
+//         lat: 33,
+//         lng: -84.23
+//       },
+//     },
+//     {
+//       name: "Location 3",
+//       location: { 
+//         lat: 33.76,
+//         lng: -84.38
+//       },
+//     },
+//     {
+//       name: "Location 4",
+//       location: { 
+//         lat: 41.3797,
+//         lng: 2.1682
+//       },
+//     },
+//     {
+//       name: "Location 5",
+//       location: { 
+//         lat: 41.4055,
+//         lng: 2.1915
+//       },
+//     }
+//   ];
   
 
 
-export default function MapContainer () {
+// export default function MapContainer () {
     
-    return (
-       <LoadScript
-         googleMapsApiKey>
-          <GoogleMap
-            mapContainerStyle={mapStyles}
-            zoom={13}
-            center={defaultCenter}
+//     return (
+//        <LoadScript
+//          googleMapsApiKey>
+//           <GoogleMap
+//             mapContainerStyle={mapStyles}
+//             zoom={13}
+//             center={defaultCenter}
 
             
-          />
+//           />
           
-       </LoadScript>
-    )
-  }
+//        </LoadScript>
+//     )
+//   }
 
+// /*
